9eb70329065e3af4b59ac4abdfebfc7fcf11edf0
Kardian <kuszawet@gmail.com>
2022-09-10 16:09:27 +0200

    Add GroupedTeamMembers, Navigator, PageNotFound, Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,11 @@ import './App.css';
 import Header from './Header'
 import Employees from './Employees'
 import Footer from './Footer'
+import GroupedTeamMembers from './GroupedTeamMembers';
+import Nav from './Nav';
+import NotFound from './NotFound';
 import { useState, useEffect } from "react";
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
 
@@ -119,18 +123,27 @@ function App() {
   }
 
   return (
-    <div>
-      <Header selectedTeam={selectedTeam}
+      <Router>
+        <Nav />
+        <Header selectedTeam={selectedTeam}
               teamMemberCount={employees.filter((employee) => employee.teamName === selectedTeam.length)} />
-      <Employees employees={employees}
-                selectedTeam={selectedTeam}
-                handleEmployeeCardClick={handleEmployeeCardClick}
-                handleTeamSelectionChange={handleTeamSelectionChange}
-      />
-      <Footer />
-    </div>
-
+      <Routes>
+        <Route path="/"
+                element={
+                  <Employees employees={employees}
+                  selectedTeam={selectedTeam}
+                  handleEmployeeCardClick={handleEmployeeCardClick}
+                  handleTeamSelectionChange={handleTeamSelectionChange}
+          />}>
+            <Route path="/GroupedTeamMembers" element={<GroupedTeamMembers employees= {employees} selectedTeam={selectedTeam} setTeam={setTeam}/>}>
+            </Route>
+            <Route path="*" element={<NotFound/>}>
+            </Route>
+        </Route>
+      </Routes>
+        <Footer />
+      </Router>
   );
 }
 
-export default App;
+export default App; 
